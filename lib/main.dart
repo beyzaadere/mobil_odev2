39927a15d5e8157073f4cef27ea096141b597247
beyzaadere <derebeyza002@gmail.com>
2024-10-17 17:14:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,35 @@
 
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //Flutter materyal kütüphanesini içe aktar
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp());  //Uygulamayı başlatır ve Myapp sınıfını çalıştırır
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ColorChangeScreen(),
-       debugShowCheckedModeBanner: false,
+      home: ColorChangeScreen(), // Ana ekran olarak ColorChangeScreen sınıfını ayarla
+       debugShowCheckedModeBanner: false, // Debug banner'ını gizle
     );
   }
 }
 
 class ColorChangeScreen extends StatefulWidget {
   @override
-  _ColorChangeScreenState createState() => _ColorChangeScreenState();
+  _ColorChangeScreenState createState() => _ColorChangeScreenState(); // Ekranın durumunu yöneten State sınıfını oluştur
 }
 
 class _ColorChangeScreenState extends State<ColorChangeScreen> {
-  Color _backgroundColor = Colors.yellow;
+  Color _backgroundColor = Colors.yellow;  // Arka plan rengini sarı olarak başlat
 
   void change_color() {
     setState(() {
 
-       _backgroundColor = (_backgroundColor == Colors.yellow)
-                ? Colors.blue.shade900
-                : Colors.yellow;
+       _backgroundColor = (_backgroundColor == Colors.yellow) // Eğer arka plan sarı ise
+                ? Colors.blue.shade900 // Lacivert yap
+                : Colors.yellow; // Değilse sarı yap
     });
   }
 
@@ -37,11 +37,11 @@ class _ColorChangeScreenState extends State<ColorChangeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      backgroundColor: _backgroundColor,
-      body: Center(
+      backgroundColor: _backgroundColor, // Arka plan rengini ayarlar
+      body: Center( // içeriği ortaya taşır
         child: ElevatedButton(
-          onPressed: change_color,
-          child: Text('Rengi Değiştirmek İçin Tıklayınız'),
+          onPressed: change_color, // Butona tıklandığında change_color fonksiyonunu çağır
+          child: Text('Rengi Değiştirmek İçin Tıklayınız'), // Buton üzerindeki metin
         ),
       ),
     );
